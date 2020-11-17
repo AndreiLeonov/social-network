824218d881f4dfa7a6bc7d234b02e7b2241a9d06
AndreiLeonov <andrei_leonov93@mail.ru>
2020-11-17 22:31:22 +0300

    useEffect added

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
 type ProfileStatusType = {
     status: string
@@ -14,6 +14,11 @@ export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
 
     const [editMode, setEditMode] = useState(false);
     const [status, setStatus] = useState(props.status);
+
+    useEffect( () => {
+        setStatus(props.status)
+
+    }, [props.status] );
 
     const activateEditMode = () => {
         setEditMode(true);
