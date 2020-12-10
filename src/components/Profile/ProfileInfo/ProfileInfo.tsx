9dd3ab0ed87fa62c617eb51a8cdf1ae3cf7b976c
AndreiLeonov <andrei_leonov93@mail.ru>
2020-12-10 23:05:06 +0300

    ProfileComponent changes > editMode for Profile

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,9 +3,16 @@ import classes from './ProfileInfo.module.css';
 import {Preloader} from "../../common/Preloader/Preloader";
 import {ProfileStatusWithHooks} from "./ProfileStatusWithHooks";
 import userPhoto from "../../../assets/images/user.png";
-
+import { ProfileDataEditForm } from "./ProfileDataForm";
 
 export function ProfileInfo(props: any) {
+
+    const [editMode, setEditMode] = React.useState(false);
+
+    const editModeOn = () => {
+        setEditMode(true);
+    }
+
     if (!props.profile) {
         return <Preloader/>
     }
@@ -19,23 +26,45 @@ export function ProfileInfo(props: any) {
     return (
         <div>
             <div className={classes.descriptionBlock}>
-                <p>{props.profile.fullName}</p>
-                <p>UserId: {props.profile.userId}</p>
-                <div><img src={props.profile.photos.small}/></div>
                 <div><img src={props.profile.photos.large || userPhoto} className={classes.MainPhoto}/></div>
+                {/*<div><img src={props.profile.photos.small}/></div>*/}
                 {props.isOwner && <input type={"file"} onChange={addPhoto}/>}
-                <p>Ищу работу: {props.profile.lookingForAJob === true ? 'да' : 'нет'}</p>
                 <div>
-                    <p>
-                        <b> Статус:
-                            <ProfileStatusWithHooks
-                                status={props.status}
-                                updateStatus={props.updateStatus}
-                            />
-                        </b>
-                    </p>
+                    <b> Статус:
+                        <ProfileStatusWithHooks
+                            status={props.status}
+                            updateStatus={props.updateStatus}
+                        />
+                    </b>
                 </div>
+                { editMode? <ProfileDataEditForm profile={props.profile}/> : <ProfileData profile={props.profile} isOwner={props.isOwner} editModeOn={editModeOn}/>}
             </div>
         </div>
     );
+}
+
+const ProfileData = (props: any) => {
+    return <div>
+        {props.isOwner && <div><button onClick={props.editModeOn}>edit</button></div>}
+        <p>{props.profile.fullName}</p>
+        <p>UserId: {props.profile.userId}</p>
+        <p>Ищу работу: {props.profile.lookingForAJob ? 'да' : 'нет'}</p>
+        {props.profile.lookingForAJob && <div><b>Навыки: </b>{props.lookingForAJobDescription}</div>}
+        <div>
+            <b>Contacts</b>: {
+            Object
+                .keys(props.profile.contacts)
+                .map((key) => {
+                    return <Contact key={key} contactTitle={key} contactValue={props.profile.contacts[key]}/>
+                })}
+        </div>
+    </div>
+}
+
+type ContactsPropsType = {
+    contactTitle: string
+    contactValue: string
+}
+export const Contact: React.FC<ContactsPropsType> = ({contactTitle, contactValue}) => {
+    return <div className={classes.contacts}><b>{contactTitle}</b>: {contactValue}</div>
 }
